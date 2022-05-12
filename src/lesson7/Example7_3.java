9397a7d19039c7ebdf1ca3b5ed3b28572d4af903
F4riK1ller <28761528@qq.com>
2022-05-12 11:29:35 +0800

    编码修改

--- src/lesson7/Example7_3.java
+++ src/lesson7/Example7_3.java
@@ -6,18 +6,18 @@ public class Example7_3{
         Calendar 日历=Calendar.getInstance();
         日历.set(1931,8,1);     //8代表九月
         int 星期几=日历.get(Calendar.DAY_OF_WEEK)-1;
-        String a[]=new String[星期几+31];  //存放号码的一维数组
+        String a[]=new String[星期几+30];  //存放号码的一维数组
         for(int i=0;i<星期几;i++){
             a[i]="";
         }
-        for(int i=星期几,n=1;i<星期几+31;i++){
+        for(int i=星期几,n=1;i<星期几+30;i++){
             a[i]=String.valueOf(n) ;
             n++;
         }
         int year=日历.get(Calendar.YEAR),
                 month=日历.get(Calendar.MONTH)+1;
         System.out.println(" "+year+"年"+month+"月"+"18日,日本发动侵华战争");
-        System.out.printf("%4c%5c%4c%5c%4c%5c%4c\n",'日','一','二', '三','四','五','六');
+        System.out.printf("%4c%4c%4c%4c%4c%4c%4c\n",'日','一','二', '三','四','五','六');
         for(int i=0;i<a.length;i++){
             if(i%7==0&&i!=0)
                 System.out.printf("\n");
